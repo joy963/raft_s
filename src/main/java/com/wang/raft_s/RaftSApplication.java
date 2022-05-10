eee9ab909b442deca718e66eea7af6eb48a232f9
wang.junhao <wang.junhao@navercrop.com>
2022-05-10 13:30:51 +0800

    init project

---
+++ src/main/java/com/wang/raft_s/RaftSApplication.java
@@ -0,0 +1,13 @@
+package com.wang.raft_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaftSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaftSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wang/raft_s/RaftSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.raft_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaftSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
