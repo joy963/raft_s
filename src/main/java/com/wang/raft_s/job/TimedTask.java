82030e3e39ca0a4740acfb0d5018c588f00eed37
wang.junhao <wang.junhao@navercrop.com>
2022-05-18 18:23:29 +0800

    modify cron job

--- src/main/java/com/wang/raft_s/job/TimedTask.java
+++ src/main/java/com/wang/raft_s/job/TimedTask.java
@@ -3,9 +3,10 @@ package com.wang.raft_s.job;
 import com.wang.raft_s.hosts.*;
 import com.wang.raft_s.rpc.*;
 import java.util.*;
+import java.util.concurrent.*;
+import javax.annotation.*;
 import lombok.extern.slf4j.*;
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.scheduling.annotation.*;
 import org.springframework.stereotype.*;
 
 @Component
@@ -15,15 +16,23 @@ public class TimedTask {
 	private HeartCheck heartCheck;
 	@Autowired
 	private HostsCache hostsCache;
+	private final ScheduledThreadPoolExecutor scheduledExecutorService = new ScheduledThreadPoolExecutor(3);
 
-	@Scheduled(cron = "5/5 * * * * ?")
+	@PostConstruct
 	public void run() {
-		Set<String> hosts = hostsCache.getHosts();
-		for (String host : hosts) {
-			boolean heart = heartCheck.heartCheck(host);
-			if (!heart) {
-				hostsCache.removeHost(host);
-				log.info("remove host: {}", host);
+		scheduledExecutorService.schedule(new HeartCheckWorker(), 5, TimeUnit.SECONDS);
+	}
+
+	class HeartCheckWorker implements Runnable {
+		@Override
+		public void run() {
+			Set<String> hosts = hostsCache.getHosts();
+			for (String host : hosts) {
+				boolean heart = heartCheck.heartCheck(host);
+				if (!heart) {
+					hostsCache.removeHost(host);
+					log.info("remove host: {}", host);
+				}
 			}
 		}
 	}
